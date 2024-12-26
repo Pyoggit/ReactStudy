2b001281d326456b546db7c4571ba6ed9aaca865
jangpyo <wkdvy122851@gmail.com>
2024-12-26 20:57:01 +0900

    [CREATE] node.js ESM 모듈 방식 테스트

---
+++ section03/src/index1.js
@@ -0,0 +1,9 @@
+//외부의 모듈 가져오기
+// const {add,sub} = require('./math');
+// console.log(add(20,20));
+// console.log(sub(10,20));
+
+//ESM 모듈 방식
+import {add,sub} from './math.js';
+console.log(add(30,20));
+console.log(sub(10,20));
