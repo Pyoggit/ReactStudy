3708a6ba75e6730b7aa6d15bbcfc3b9e21ad6aec
jangpyo <wkdvy122851@gmail.com>
2024-12-27 12:23:42 +0900

    [UPDATE] 표기가능 변수 테스트

--- section04/src/components/Main.jsx
+++ section04/src/components/Main.jsx
@@ -1,9 +1,21 @@
 function Main(){
+
+    const number = 9;
+    const obj = {name: "jp", age: 27};
+
     return(
-      <main>
-        <h1>메인</h1>
-      </main>
+       <>
+       <h1>Main</h1>
+       <h2>삼항연산자가능 = {number % 2 === 0? "짝수":"홀수"}</h2>
+       <h2>숫자표기가능 = {10}</h2>
+       <h2>number = {number}</h2>
+       <h2>[1,2,3] = {[1,2,3]}</h2>
+       <h2>true = {true}</h2>
+       <h2>undefined = {undefined}</h2>
+       <h2>null = {null}</h2>
+       <h2>obj.name = {obj.name}</h2>
        
+       </>
     );
   }
 
