99933c36d16d3148c2dfa1823f9cfd0973edf019
jangpyo <wkdvy122851@gmail.com>
2024-12-27 12:34:49 +0900

    [CREATE] React css 적용 테스트

--- section04/src/components/Main.jsx
+++ section04/src/components/Main.jsx
@@ -1,22 +1,15 @@
+import "./Main.css"
+
 function Main(){
 
     const number = 9;
-    const obj = {name: "jp", age: 27};
+    const obj = {name: "jp", age: 27, isLogin: false};
 
-    return(
-       <>
-       <h1>Main</h1>
-       <h2>삼항연산자가능 = {number % 2 === 0? "짝수":"홀수"}</h2>
-       <h2>숫자표기가능 = {10}</h2>
-       <h2>number = {number}</h2>
-       <h2>[1,2,3] = {[1,2,3]}</h2>
-       <h2>true = {true}</h2>
-       <h2>undefined = {undefined}</h2>
-       <h2>null = {null}</h2>
-       <h2>obj.name = {obj.name}</h2>
-       
-       </>
-    );
-  }
+    if(obj.isLogin === true){
+        return(<div className="logout">{obj.name} 로그아웃</div>);
+    }else{
+        return(<div className="login">{obj.name} 로그인</div>);       
+    }
+  };
 
   export default Main;
