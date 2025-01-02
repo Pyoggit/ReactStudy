e6048ead78c0d9b85e13e03f8b210aa28bae3721
jangpyo <wkdvy122851@gmail.com>
2025-01-02 22:41:35 +0900

    [UPDATE] 작성자,이메일 입력칸 추가

--- section07/src/components/Editor.jsx
+++ section07/src/components/Editor.jsx
@@ -3,19 +3,26 @@ import { useState, useRef } from "react";
 
 const Editor = ({ onInsert }) => {
   const [content, setContent] = useState("");
-  const inputRef = useRef();
+  const [writer, setWriter] = useState("");
+  const [email, setEmail] = useState("");
+  const contentRef = useRef();
+  const writerRef = useRef();
+  const emailRef = useRef();
+
   const onSubmit = () => {
-    if (content === "") {
-      alert("내용을 입력해주세요");
-      inputRef.current.focus();
+    if (content === "" || writer === "" || email === "") {
+      alert("내용과 작성자를 모두 입력해주세요.");
+      if (content === "") contentRef.current.focus();
+      if (writer === "") writerRef.current.focus();
+      if (email === "") emailRef.current.focus();
       return;
     }
-    onInsert(content);
+    onInsert(content, writer, email);
     setContent("");
+    setWriter("");
+    setEmail("");
   };
-  const onChangeContent = (e) => {
-    setContent(e.target.value);
-  };
+
   const onKeyDown = (e) => {
     if (e.keyCode === 13) {
       onSubmit();
@@ -25,12 +32,28 @@ const Editor = ({ onInsert }) => {
   return (
     <div className="editor">
       <input
-        value={content}
-        ref={inputRef}
+        value={writer}
+        ref={writerRef}
         type="text"
-        onChange={onChangeContent}
+        onChange={(e) => setWriter(e.target.value)}
         onKeyDown={onKeyDown}
-        placeholder="새로운 할일 입력요망"
+        placeholder="작성자"
+      />
+      <input
+        value={email}
+        ref={emailRef}
+        type="text"
+        onChange={(e) => setEmail(e.target.value)}
+        onKeyDown={onKeyDown}
+        placeholder="이메일"
+      />
+      <input
+        value={content}
+        ref={contentRef}
+        type="text"
+        onChange={(e) => setContent(e.target.value)}
+        onKeyDown={onKeyDown}
+        placeholder="할 일을 입력하세요"
       />
       <button onClick={onSubmit}>추가</button>
     </div>
