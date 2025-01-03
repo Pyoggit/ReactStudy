d9fe2ee4bd62c0acfbc457063c6f1f8a51d846a5
jangpyo <wkdvy122851@gmail.com>
2025-01-03 12:04:22 +0900

    [UPDATE] 이미지 테스트

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -1,39 +1,90 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
+import New from "./pages/New";
 import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
-import New from "./pages/NEw";
+import { getEmotionImage } from "./util/get-emotion-images";
+import img1 from "./assets/emotion1.png";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   const nav = useNavigate();
-
-  //이벤트 처리방식으로 페이지 이동한다.
-  const onClickButton = () => {
-    nav("/new");
-  };
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <>
+      <Header
+        left={<Button text={"<"} type={"GREEN"} />}
+        title={"Header"}
+        right={<Button text={">"} type={"RED"} />}
+      />
+
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
         <Link to={"/edit"}>Edit</Link>
         <br />
+
         <a href="/">Home</a>
         <a href="/new">New</a>
         <a href="/diary">Diary</a>
         <a href="/edit">Edit</a>
-      </div>
+        <br />
 
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+        <button
+          onClick={() => {
+            nav("/");
+          }}
+        >
+          Home
+        </button>
+        <button
+          onClick={() => {
+            nav("/new");
+          }}
+        >
+          New
+        </button>
+        <button
+          onClick={() => {
+            nav("/diary");
+          }}
+        >
+          Diary
+        </button>
+        <button
+          onClick={() => {
+            nav("/edit");
+          }}
+        >
+          edit
+        </button>
+        <br />
+
+        <img src={getEmotionImage(1)} alt="감정" />
+        <img src={getEmotionImage(2)} alt="감정" />
+        <img src={getEmotionImage(3)} alt="감정" />
+        <img src={getEmotionImage(4)} alt="감정" />
+        <img src={getEmotionImage(5)} alt="감정" />
+        <br />
+        <img src={img1} alt="감정" />
+        <img src={"/emotion1.png"} alt="감정" />
+      </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="/edit" element={<Edit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
