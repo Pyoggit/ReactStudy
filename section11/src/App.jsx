2d3e307168c7f946cce71c179e3d8af7bdf1b886
jangpyo <wkdvy122851@gmail.com>
2025-01-03 09:48:00 +0900

    [UPDATE] Link와 a 의 페이지 이동 속도 테스트

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
@@ -7,8 +7,29 @@ import NotFound from "./pages/NotFound";
 import New from "./pages/NEw";
 
 function App() {
+  const nav = useNavigate();
+
+  //이벤트 처리방식으로 페이지 이동한다.
+  const onClickButton = () => {
+    nav("/new");
+  };
+
   return (
     <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+        <Link to={"/edit"}>Edit</Link>
+        <br />
+        <a href="/">Home</a>
+        <a href="/new">New</a>
+        <a href="/diary">Diary</a>
+        <a href="/edit">Edit</a>
+      </div>
+
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
