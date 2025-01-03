c7322c39efc2a078952c526a2c31a2b1029a21c7
jangpyo <wkdvy122851@gmail.com>
2025-01-03 20:14:55 +0900

    [CREATE] 새 글 작성 페이지 및 기능 구현

--- section11/src/pages/New.jsx
+++ section11/src/pages/New.jsx
@@ -1,8 +1,33 @@
+import Header from "../components/Header";
+import Editor from "../components/Editor";
+import Button from "../components/Button";
+import { DiaryDispatchContext } from "../App";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 const New = () => {
+  const nav = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
   return (
-    <>
-      <h1>New</h1>
-    </>
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        left={
+          <Button
+            onClick={() => {
+              nav(-1);
+            }}
+            text={"< 뒤로 가기"}
+          />
+        }
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
   );
 };
 
