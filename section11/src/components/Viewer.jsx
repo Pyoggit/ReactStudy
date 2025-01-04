4ea4e925859d5faf90549014b8f9d087713020c0
jangpyo <wkdvy122851@gmail.com>
2025-01-04 13:11:21 +0900

    [UPDATE] props 전달 문제 해결

--- section11/src/components/Viewer.jsx
+++ section11/src/components/Viewer.jsx
@@ -2,18 +2,25 @@ import "./Viewer.css";
 import { getEmotionImage } from "../util/get-emotion-images";
 import { emotionList } from "../util/constants";
 
-const Viewer = (emotionId, content) => {
-  // src/util/constants.js 가져오기
+const Viewer = ({ emotionId, content }) => {
+  console.log("Emotion ID:", emotionId);
+  console.log("Content:", content);
+
   const emotionItem = emotionList.find(
     (item) => String(item.emotionId) === String(emotionId)
   );
+  console.log("Emotion Item:", emotionItem);
+
+  if (!emotionItem) {
+    return <div>감정을 찾을 수 없습니다.</div>;
+  }
 
   return (
     <div className="Viewer">
       <section className="img_section">
         <h4>오늘의 감정</h4>
         <div className="emotion_img_wrapper">
-          <img src={getEmotionImage(emotionItem.emotionId)} />
+          <img src={getEmotionImage(emotionItem.emotionId)} alt="Emotion" />
           <div>{emotionItem.emotionName}</div>
         </div>
       </section>
